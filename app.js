f39322f284b773d3c45d76ca7bd688e9db53c55c
AliciaGlenn <Alicia@AliciaGlenn.com>
2023-02-17 21:19:48 -0500

    css updates

--- app.js
+++ app.js
@@ -64,6 +64,25 @@ function render() {
 
 const pokeGallery_container = document.getElementById("pokeGallery_container");
 const pokemons_number = 150;
+const pokeColors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const main_types = Object.keys(pokeColors);
+console.log(main_types);
 
 // looping through all pokemon to show up to 150 pokemon
 const fetchPokemons = async () => {
@@ -84,14 +103,19 @@ fetchPokemons();
 function createPokemonCard(pokemon) {
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
-
+  const poke_Types = pokemon.types.map((el) => el.type.name);
+  const type = main_types.find((type) => poke_Types.indexOf(type) - 1);
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-
+  const color = pokeColors[type];
+  pokemonEl.style.background = color;
   const pokeInnerHTML = `
   <div class ="img-container">
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png"
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png">
   </div>
-    ${name}
+    <div class = "info">
+    <span class ="number"> ${pokemon.id}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type"> Type: <span> ${type} </span> </small>
   `;
   pokemonEl.innerHTML = pokeInnerHTML;
   pokeGallery_container.appendChild(pokemonEl);
